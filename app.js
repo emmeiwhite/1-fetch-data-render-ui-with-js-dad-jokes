f4db774c32a841bde401c4afe263dd6e9dd8217f
emmeiwhite <emmeiwhite@gmail.com>
2024-10-10 14:53:59 +0530

    async-await with fetch() network error handling

--- app.js
+++ app.js
@@ -7,4 +7,27 @@ const result = document.querySelector('.result')
 btn.addEventListener('click', () => {
   console.log('Result is')
   console.log(result)
+
+  fetchDadJokes()
 })
+
+const fetchDadJokes = async () => {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'learning app'
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error('Error: Could not fetch data!')
+    }
+
+    const data = await response.json()
+
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
